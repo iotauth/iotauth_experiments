82993d3ef1b599dd79724429c56fda46a5ce9ba2
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-05-06 18:25:39 -0700

    implemented packet result analyzer

--- experiments/ccs2017/PCAP_README.md
+++ experiments/ccs2017/PCAP_README.md
@@ -11,9 +11,10 @@ ip.dst == 10.0.1.8
 
 ip.dst == 10.0.1.8 && ip.src
 
+(ip.src == 10.0.1.10  && ip.dst == 10.0.1.3) || (arp.src.proto_ipv4 == 10.0.1.10 && arp.dst.proto_ipv4 == 10.0.1.3)
 # For analyzing packets captured
 
-node packetAnalyzer.js [DIRECTORY] [GRAPH] [DEV_LIST] [COMM_COST]
+node packetAnalyzer.js [RESULT_DIRECTORY] [GRAPH] [DEV_LIST] [COMM_COST]
 
 node packetAnalyzer.js $NS3 $CONF/ns3Exp.graph $CONF/devList.txt $CONF/commCosts.txt
 
--- experiments/ccs2017/packetAnalyzer.js
+++ experiments/ccs2017/packetAnalyzer.js
@@ -22,6 +22,9 @@
 
 var fs = require('fs');
 var util = require('util');
+const execSync = require('child_process').execSync;
+
+var debugging = true;
 
 function getTapName(devName) {
     return 'tap-' + devName;
@@ -29,7 +32,7 @@ function getTapName(devName) {
 
 // get files
 if (process.argv.length <= 5) {
-    console.error('[Pcap directory], [graph file], [dev list file] and [comm costs file] must be provided!');
+    console.error('[Pcap result directory], [graph file], [dev list file] and [comm costs file] must be provided!');
     process.exit(1);
 }
 var pcapDir = process.argv[2];
@@ -66,13 +69,19 @@ for (var i = 0; i < devList.length; i++) {
         addrMap[dev.name] = dev.wifi;
     }
     else {
+            var pcapInfo = {
+                name: dev.name,
+                tapName: getTapName(dev.name),
+                addr: dev.addr,
+                targetList: []
+            };
         if (dev.type === 'server') {
             addrMap[dev.name] = dev.addr;
-            serverMap[dev.name] = {name: dev.name, tapName: getTapName(dev.name), targetList: []};
+            serverMap[dev.name] = pcapInfo;
         }
         else if (dev.type === 'client') {
             addrMap[dev.name] = dev.addr;
-            clientMap[dev.name] = {name: dev.name, tapName: getTapName(dev.name), targetList: []};
+            clientMap[dev.name] = pcapInfo;
         }
     }
 }
@@ -136,11 +145,158 @@ for (var i = 0; i < entityList.length; i++) {
 //console.log(util.inspect(clientMap, {depth:3}));
 //console.log(util.inspect(serverMap, {depth:3}));
 
-var pcapList = Object.values(clientMap).concat(Object.values(serverMap));
-pcapList = pcapList.sort(function(a, b) {
+var pcapInfoList = Object.values(clientMap).concat(Object.values(serverMap));
+pcapInfoList = pcapInfoList.sort(function(a, b) {
   return a.name < b.name ? -1 : 1;
 });
-console.log(util.inspect(pcapList, {depth:3}));
+
+if (debugging) {
+    // For debugging pcap entity-target addresses
+    console.log('\nFor debugging pcap entity-target addresses: \n');
+    console.log(util.inspect(pcapInfoList, {depth:3}));
+}
+
+// ip.src == 10.0.1.8
+// arp.src.proto_ipv4 == 10.0.1.8
+
+// ip.dst == 10.0.1.3
+// arp.dst.proto_ipv4 == 10.0.1.3
+
+// Example pcap info
+//[ { name: 't1',
+//    tapName: 'tap-t1',
+//    addr: '10.0.1.5',
+//    targetList: 
+//     [ { name: 'auth1', addr: '10.0.1.1', cost: 1 },
+//       { name: 'auth3', addr: '10.0.1.3', cost: 2.5 },
+//       { name: 't2', addr: '10.0.1.8', cost: 1 },
+//       { name: 't5', addr: '10.0.1.10', cost: 1 } ] },
+
+function getPcapFilter(srcAddr, dstAddr) {
+    var pcapFilter = '(ip.src == SRC_ADDR && ip.dst == DST_ADDR) || ' +
+        '(arp.src.proto_ipv4 == SRC_ADDR && arp.dst.proto_ipv4 == DST_ADDR)';
+    pcapFilter = pcapFilter.replace(new RegExp('SRC_ADDR', 'g'), srcAddr);
+    pcapFilter = pcapFilter.replace(new RegExp('DST_ADDR', 'g'), dstAddr);
+    return pcapFilter;
+}
+
+var pcapAnalysisList = [];
+for (var i = 0; i < pcapInfoList.length; i++) {
+    var pcapInfo = pcapInfoList[i];
+    var pcapFileName = pcapDir + '/wifi-' + pcapInfo.tapName + '.pcap';
+    var countCommandTemplate = 'tshark -2 -nr "' + pcapFileName + '" -R "PCAP_FILTER" -T fields -e frame.len | wc -l';
+    var bytesCommandTemplate = 'tshark -2 -nr "' + pcapFileName + '" -R "PCAP_FILTER" -T fields -e frame.len | awk \'{sum+=$1}END{print sum}\'';
+    var analysisTargetList = [];
+    for (var j = 0; j < pcapInfo.targetList.length; j++) {
+        var target = pcapInfo.targetList[j];
+        var myAddr = pcapInfo.addr;
+        var targetAddr = target.addr;
+        var sentFilter = getPcapFilter(myAddr, targetAddr);
+        var receivedFilter = getPcapFilter(targetAddr, myAddr);
+        analysisTargetList.push({
+            name: target.name,
+            cost: target.cost,
+            sentCoundCommand: countCommandTemplate.replace('PCAP_FILTER', sentFilter),
+            sentBytesCommand: bytesCommandTemplate.replace('PCAP_FILTER', sentFilter),
+            receivedCoundCommand: countCommandTemplate.replace('PCAP_FILTER', receivedFilter),
+            receivedBytesCommand: bytesCommandTemplate.replace('PCAP_FILTER', receivedFilter)
+        });
+    }
+    pcapAnalysisList.push({
+        name: pcapInfo.name,
+        targetList: analysisTargetList
+    });
+}
+
+if (debugging) {
+    // For debugging pcap commands
+    console.log('\nFor debugging pcap commands: \n');
+    console.log(util.inspect(pcapAnalysisList, {depth:3}));
+}
+
+function getTsharkResult(result) {
+    result = result.toString().trim();
+    if (result.length == 0) {
+        return 0;
+    }
+    return parseInt(result);
+}
+
+var entityPcapResultList = [];
+
+console.log('\nProcessing pcap results...\n');
+for (var i = 0; i < pcapAnalysisList.length; i++) {
+    var pcapAnalysis = pcapAnalysisList[i];
+    var results = [];
+    for (var j = 0; j < pcapAnalysis.targetList.length; j++) {
+        var target = pcapAnalysis.targetList[j];
+        console.log('EntityName: ' + pcapAnalysis.name + '\tTargetName: ' + target.name);
+        var data = {
+            sentCount: getTsharkResult(execSync(target.sentCoundCommand)),
+            sentBytes: getTsharkResult(execSync(target.sentBytesCommand)),
+            receivedCount: getTsharkResult(execSync(target.receivedCoundCommand)),
+            receivedBytes: getTsharkResult(execSync(target.receivedBytesCommand))
+        };
+        results.push({
+            targetName: target.name,
+            cost: target.cost,
+            data: data
+        });
+    }
+    entityPcapResultList.push({
+        entityName: pcapAnalysis.name,
+        results: results
+    });
+}
+
+if (debugging) {
+    // For debugging pcap raw results
+    console.log('\nFor debugging pcap raw results: \n');
+    console.log(util.inspect(entityPcapResultList, {depth:4}));
+}
+
+function convertPcapResultToEnergy(data, cost) {
+    // Energy per operation
+    // Send packet: 454 μJ + 1.9 μJ × packet size (bytes)
+    // Receive packet: 356 μJ + 0.5 μJ × packet size (bytes)
+    var energyForSending = 454 * data.sentCount + 1.9 * data.sentBytes;
+    var energyForReceiving = 356 * data.receivedCount + 0.5 * data.receivedBytes;
+    return energyForSending * cost + energyForReceiving;
+}
+
+var entityEnergyResultList = [];
+for (var i = 0; i < entityPcapResultList.length; i++) {
+    var entityPcapResult = entityPcapResultList[i];
+    var energyResults = [];
+    var totalEnergy = 0;
+    for (var j = 0; j < entityPcapResult.results.length; j++) {
+        var pcapResult = entityPcapResult.results[j];
+        var energyResult = convertPcapResultToEnergy(pcapResult.data, pcapResult.cost);
+        energyResults.push({
+            targetName: pcapResult.targetName,
+            energy: energyResult
+        });
+        totalEnergy += energyResult;
+    }
+    entityEnergyResultList.push({
+        entityName: entityPcapResult.entityName,
+        totalEnergy: totalEnergy,
+        results: energyResults
+    });
+}
+
+console.log('\nDisplaying final results: \n');
+console.log(util.inspect(entityEnergyResultList, {depth:3}));
+
+var totalThingsEnergy = 0;
+for (var i = 0; i < entityEnergyResultList.length; i++) {
+    var entityEnergyResult = entityEnergyResultList[i];
+    totalThingsEnergy += entityEnergyResult.totalEnergy;
+}
+console.log('\nTotal energy for all things: \n');
+console.log(totalThingsEnergy / 1000 + ' mJ');
+
+
 
 
 
