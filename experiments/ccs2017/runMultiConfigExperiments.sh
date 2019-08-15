e60531fb5cd2d5f47358fe0f4ea423dcc68d58ed
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2019-08-14 20:33:56 -0700

    Default without less Naive

--- experiments/ccs2017/runMultiConfigExperiments.sh
+++ experiments/ccs2017/runMultiConfigExperiments.sh
@@ -3,11 +3,93 @@
 # Script for running experiments with multiple configurations - from generating Auths/Servers/Clients.
 # Author: Hokeun Kim
 
-# ORDER_ID="order1"
-# AUTH_KILL_ORDER="504,402,501,403,503,401"
+ORDER_ID="order1"
+AUTH_KILL_ORDER="504,402,501,403,503,401"
 
-# ORDER_ID="order2"
-# AUTH_KILL_ORDER="501,403,504,401,503,402"
+### Template for None, Naive, ILP, ILP_mt_ac experiments ###
+echo $ORDER_ID
+echo $AUTH_KILL_ORDER
+
+echo $ORDER_ID - None
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$ORDER_ID/cory45_ILP.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP_mt_ac
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
+############################################################
+
+ORDER_ID="order2"
+AUTH_KILL_ORDER="501,403,504,401,503,402"
+
+### Template for None, Naive, ILP, ILP_mt_ac experiments ###
+echo $ORDER_ID
+echo $AUTH_KILL_ORDER
+
+echo $ORDER_ID - None
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$ORDER_ID/cory45_ILP.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP_mt_ac
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
+############################################################
 
 ORDER_ID="order3"
 AUTH_KILL_ORDER="503,504,402,403,401,501"
