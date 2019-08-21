1abf410339ae5c3cda6ce87235c90fb03c13a036
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2019-08-20 22:45:21 -0700

    Put order2 and order 4 at the end of experiments, ready for trust3 experiments

--- experiments/ccs2017/runMultiConfigExperiments.sh
+++ experiments/ccs2017/runMultiConfigExperiments.sh
@@ -7,8 +7,11 @@
 #TRUST_ID="trust1"
 #AUTH_TRUSTS="501-502,501-504,502-503,502-504,401-402,401-403,401-502,401-504,402-503,403-501"
 
-TRUST_ID="trust2"
-AUTH_TRUSTS="501-502,503-504,502-503,502-504,401-402,401-403,401-502,401-504,402-501,403-503"
+#TRUST_ID="trust2"
+#AUTH_TRUSTS="501-502,503-504,502-503,502-504,401-402,401-403,401-502,401-504,402-501,403-503"
+
+TRUST_ID="trust3"
+AUTH_TRUSTS="501-502,501-504,502-503,502-504,403-402,401-403,401-502,401-501,402-502,503-504"
 
 mkdir -p floorPlans/cory45/$TRUST_ID
 mkdir -p $CONF/$TRUST_ID
@@ -84,114 +87,8 @@ cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.
 ./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
 ########################################################################
 
-ORDER_ID="order2"
-AUTH_KILL_ORDER="501,403,504,401,503,402"
-
-### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
-echo $ORDER_ID
-echo $AUTH_KILL_ORDER
-
-echo $ORDER_ID - None
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - Naive
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - Less_Naive
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45_less_naive.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Less_Naive -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - ILP
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - ILP_mt_ac
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
-########################################################################
-
 ORDER_ID="order3"
 AUTH_KILL_ORDER="503,504,402,403,401,501"
-
-### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
-echo $ORDER_ID
-echo $AUTH_KILL_ORDER
-
-echo $ORDER_ID - None
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - Naive
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - Less_Naive
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/cory45_less_naive.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Less_Naive -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - ILP
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
-
-echo $ORDER_ID - ILP_mt_ac
-
-cd $IOT/examples
-./cleanAll.sh
-./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
-cd $CCS
-cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
-./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
-########################################################################
-
-ORDER_ID="order4"
-AUTH_KILL_ORDER="402,503,504,401,501,403"
 
 ### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
 echo $ORDER_ID
@@ -298,6 +195,112 @@ cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.
 
 ORDER_ID="order6"
 AUTH_KILL_ORDER="401,504,402,403,501,503"
+
+### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
+echo $ORDER_ID
+echo $AUTH_KILL_ORDER
+
+echo $ORDER_ID - None
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Less_Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45_less_naive.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Less_Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP_mt_ac
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
+########################################################################
+
+ORDER_ID="order2"
+AUTH_KILL_ORDER="501,403,504,401,503,402"
+
+### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
+echo $ORDER_ID
+echo $AUTH_KILL_ORDER
+
+echo $ORDER_ID - None
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_disabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/None -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - Less_Naive
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/cory45_less_naive.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/Less_Naive -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP -m 6 -o $AUTH_KILL_ORDER
+
+echo $ORDER_ID - ILP_mt_ac
+
+cd $IOT/examples
+./cleanAll.sh
+./generateAll.sh -g $CONF/$TRUST_ID/$ORDER_ID/cory45_ILP_mt_ac.graph -p asdf
+cd $CCS
+cp $CCS/expOptions/expEntity_migration_enabled.option $CCS/expOptions/expEntity.option
+./runMultipleExperiments.sh -e $TRUST_ID/$ORDER_ID/ILP_mt_ac -m 6 -o $AUTH_KILL_ORDER
+########################################################################
+
+ORDER_ID="order4"
+AUTH_KILL_ORDER="402,503,504,401,501,403"
 
 ### Template for None, Naive, Less_Naive, ILP, ILP_mt_ac experiments ###
 echo $ORDER_ID
